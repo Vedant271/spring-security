f16802e5367a4d646297424f9d8ef79ce6c09482
Vedant Karale <vedantkarale271@gmail.com>
2025-04-15 00:23:14 +0530

    sessions

--- src/main/java/com/javaproject/security/spring_security/controller/HelloController.java
+++ src/main/java/com/javaproject/security/spring_security/controller/HelloController.java
@@ -1,12 +1,18 @@
 package com.javaproject.security.spring_security.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
     @GetMapping("/hello")
-    public String hello(){
-        return "Hello World";
+    public String hello(HttpServletRequest request){
+        return "Hello World "+request.getSession().getId();
+    }
+
+    @GetMapping("/home")
+    public String home(HttpServletRequest request){
+        return "Welcome to home "+request.getSession().getId();
     }
 }
