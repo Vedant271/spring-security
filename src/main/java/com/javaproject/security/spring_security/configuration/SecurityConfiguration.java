6b4ed930eb57b03e1ea10004db39b00323f7141a
Vedant Karale <vedantkarale271@gmail.com>
2025-04-15 13:40:58 +0530

    Security Configuration, building security filter chain without configuration, direct access

---
+++ src/main/java/com/javaproject/security/spring_security/configuration/SecurityConfiguration.java
@@ -0,0 +1,18 @@
+package com.javaproject.security.spring_security.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration {
+    @Bean
+    public SecurityFilterChain buildSecurityFilterChain(HttpSecurity httpSecurity) throws Exception {
+
+        return httpSecurity.build();
+    }
+}
