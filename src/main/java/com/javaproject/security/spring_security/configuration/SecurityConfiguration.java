3d8acf2c76a79248a9f11d37fa13fa47546b2153
Vedant Karale <vedantkarale271@gmail.com>
2025-04-16 00:03:04 +0530

    Disabling CSRF Token

--- src/main/java/com/javaproject/security/spring_security/configuration/SecurityConfiguration.java
+++ src/main/java/com/javaproject/security/spring_security/configuration/SecurityConfiguration.java
@@ -3,8 +3,10 @@ package com.javaproject.security.spring_security.configuration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -12,6 +14,11 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfiguration {
     @Bean
     public SecurityFilterChain buildSecurityFilterChain(HttpSecurity httpSecurity) throws Exception {
+
+        httpSecurity.csrf(customizer -> customizer.disable());
+        httpSecurity.authorizeHttpRequests(request -> request.anyRequest().authenticated());
+        httpSecurity.httpBasic(Customizer.withDefaults());
+        httpSecurity.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
 
         return httpSecurity.build();
     }
